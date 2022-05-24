4820fd8f972c60cc619f06e555d2f0be465980e5
Jason <timeli@naver.com>
2022-05-24 12:02:59 +0900

    포트 변경

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connect = require('./schemas');
 const app = express();
-const port = 5001;
+const port = 3000;
 const articleRouter = require('./routes/article.js');
 
 connect();
